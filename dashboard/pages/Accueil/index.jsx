6ebe7c7ee182d3b0935a57409f4476dcd0aa0ee3
Fonji Terence <terencehilln@gmail.com>
2023-04-04 17:03:45 +0200

    Client

--- dashboard/pages/Accueil/index.jsx
+++ dashboard/pages/Accueil/index.jsx
@@ -12,10 +12,10 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Accueil", href: "#", current: true },
-  { name: "Clients", href: "#", current: false },
-  { name: "Factures", href: "#", current: false },
-  { name: "Paramètres", href: "#", current: false },
+  { name: "Accueil", href: "/", current: true },
+  { name: "Clients", href: "/Clients", current: false },
+  { name: "Factures", href: "/Factures", current: false },
+  { name: "Paramètres", href: "/Paramètres", current: false },
 ];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
